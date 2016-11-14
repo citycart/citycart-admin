e6650d62860368796d3f3dc3a0b8f11afee4045f
OTM <tm.oubelkacem@gmail.com>
2016-11-14 23:25:54 +0100

    init commit

---
+++ src/main/java/com/citycart/admin/backend/CitycartAdminApplication.java
@@ -0,0 +1,13 @@
+package com.citycart.admin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitycartAdminApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CitycartAdminApplication.class, args);
+	}
+}
